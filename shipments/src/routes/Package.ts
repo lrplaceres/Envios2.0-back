4ff50e974db35c6432108d882d783ef52206e3c1
Lázaro Placeres <lrodriguezplaceres@gmail.com>
2024-10-12 21:07:27 -0400

    feat models shipments

---
+++ shipments/src/routes/Package.ts
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const packageSchema = new Schema(
+  {
+    shipment: {
+        type: Schema.Types.ObjectId,
+        require: true,
+        ref: "Shipment",
+      },
+    type: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    weigth: {
+      type: Number,
+      trim: true,
+    },
+    value_equipment: {
+        type: Number,
+        trim: true,
+      },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+export default model("Municipality", packageSchema);
---
+++ shipments/src/routes/Shipment.ts
@@ -0,0 +1,96 @@
+import { Schema, model } from "mongoose";
+
+const shipmentSchema = new Schema(
+  {
+    client: {
+      type: Schema.Types.ObjectId,
+      require: true,
+      ref: "Client",
+    },
+    receipent: {
+      type: Schema.Types.ObjectId,
+      require: true,
+      ref: "Receipent",
+    },
+    office: {
+      type: Schema.Types.ObjectId,
+      require: true,
+      ref: "Office",
+    },
+    packages_total: {
+      type: Number,
+      require: true,
+    },
+    facture_number: {
+      type: Number,
+      require: true,
+      unique: true,
+    },
+    status: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    code: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    province: {
+      type: Schema.Types.ObjectId,
+      require: true,
+      ref: "Province",
+    },
+    municipality: {
+      type: Schema.Types.ObjectId,
+      require: true,
+      ref: "Municipality",
+    },
+    value_total: {
+      type: Number,
+      require: true,
+    },
+    value_aduana: {
+      type: Number,
+      require: true,
+    },
+    value_shipment: {
+      type: Number,
+      require: true,
+    },
+    weigth: {
+      type: Number,
+      require: true,
+    },
+    date: {
+      type: Date,
+      require: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    status_delivery: {
+      type: String,
+      trim: true,
+      require: true,
+    },
+    date_acepted_delivery: {
+      type: Date,
+    },
+    date_delivery: {
+      type: Date,
+    },
+    description_delivery: {
+      type: String,
+      trim: true,
+    },
+    photo_delivery: {
+      type: String,
+      trim: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+export default model("Shipment", shipmentSchema);
